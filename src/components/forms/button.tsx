dc610c29ed5438676cc80307cbd27a4fec86f5b4
TomDevBr <tomasjocalheiros@gmail.com>
2025-04-06 20:03:05 -0300

    aulas loginForm/useForm/input

---
+++ src/components/forms/button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from "./button.module.css";
+
+type ButtonType =  React.ButtonHTMLAttributes<HTMLButtonElement> ;
+
+
+ const Button =  ({children, ...props}: ButtonType) => {
+    return (
+        <button className={styles.button} {...props}>
+            {children}
+        </button>
+    )
+}
+
+export default Button;
--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,9 +1,49 @@
-export default async function LoginForm() {
+'use client';
+
+import { useFormState, useFormStatus } from "react-dom";
+import Button from "../forms/button";
+import login from "@/actions/login";
+import Input from "../forms/input";
+import ErrorMessage from "../helper/error-message";
+import { useEffect } from "react";
+
+function FormButton() {
+  const { pending } = useFormStatus();
+
   return (
-    <form action="">
-      <input type="text" name="username" placeholder="usuário" />
-      <input type="password" name="password" placeholder="senha" />
-      <button>Entrar</button>
-    </form>
+    <>
+      {pending ? (
+        <Button disabled={pending}>Enviando...</Button>
+      ) : (
+        <Button>Entrar</Button>
+      )}
+    </>
+  );
+}
+
+export default function LoginForm() {
+  const [state, action] = useFormState(login, {
+    ok: false,
+    error: '',
+    data: null,
+  });
+
+  useEffect(() => {
+    if (state.ok) {
+      window.location.href = '/conta';
+    }
+  }, [state.ok]);
+
+  return (
+    <>
+      <form action={action}>
+        <Input label={"Usuário"} name={"username"} type="text"/>
+        <Input label={"Senha"} name={"password"} type="password"/>
+
+        <ErrorMessage error={state.error} />
+        <FormButton />
+      
+      </form>
+    </>
   );
 }
